8d9ea3d8bfcfbe53f4498bd93ee224bd9aac9d6f
StevenD <dorvilsteven@gmail.com>
2022-01-12 04:54:09 -0500

    update server.js file with mongoose.connect

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
